dec908d66ece5c60cda3fb3b9cee803f017a0c90
sheikhhasib <hasib2130@gmail.com>
2021-08-03 15:09:16 +0600

    ninja list tiny project done

--- comps/Navbar/Navbar.js
+++ comps/Navbar/Navbar.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/Image';
 
 const Navbar = () => {
     return (
         <nav>
             <div className="logo">
-                <Image src="/logo.png" width={128} height={77} alt="" />
+                <img src="/logo.png" width="128px" height="77" alt="" />
             </div>
             <Link href="/"><a>Home</a></Link>
             <Link href="/about"><a>About</a></Link>
